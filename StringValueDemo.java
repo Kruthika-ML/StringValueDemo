fffc9ab60aa192784f218dee2f28280341421605
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-16 11:50:12 +0530

    Add files via upload

---
+++ StringValueDemo.java
@@ -0,0 +1,9 @@
+public class StringValueDemo {
+    public static void main(String[] args){
+     String statement = "This is a statement";
+     String statement1 = new String("Another statement");
+        System.out.println(statement);
+        System.out.println(statement1);
+    }
+}
+//To store sequence of charecters
